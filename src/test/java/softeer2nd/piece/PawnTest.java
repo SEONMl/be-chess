ae0ebfefcd9bfa3507716c4df118971f12833750
SEONMl <seonmi@bagseonmiui-MacBookPro.local>
2023-07-03 17:50:54 +0900

    test: 검정색 폰 생성 후 getColor 메서드 검증하는 테스트 코드 추가

--- src/test/java/softeer2nd/piece/PawnTest.java
+++ src/test/java/softeer2nd/piece/PawnTest.java
@@ -7,9 +7,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PawnTest {
     @Test
-    @DisplayName("흰 색 폰이 생성되어야 한다.")
-    public void createWhitePawn(){
+    @DisplayName("색깔을 가진 폰이 생성되어야 한다.")
+    public void createPawn(){
         Pawn pawn = new Pawn("white");
+        Pawn blackPawn = new Pawn("black");
+
         assertThat(pawn.getColor()).isEqualTo("white");
+        assertThat(blackPawn.getColor()).isEqualTo("black");
     }
 }
+
+// 주간 스터디 플랜, 먼저 만들기
