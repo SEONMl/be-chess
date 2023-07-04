7ebbb4e07f662a1dfb74c1e7ce413a326160d022
Seonmi Park <seonmi@bagseonmiui-MacBookPro.local>
2023-07-04 16:28:37 +0900

    feat: Pawn 클래스에 출력 문자 필드 추가와 테스트 코드 수정

--- src/main/java/softeer2nd/domain/ChessColor.java
+++ src/main/java/softeer2nd/domain/ChessColor.java
@@ -1,6 +1,17 @@
 package softeer2nd.domain;
 
 public enum ChessColor {
-    WHITE,
-    BLACK,
+    WHITE('p'),
+    BLACK('P'),
+    ;
+
+    private final char representation;
+
+    ChessColor(char p) {
+        this.representation = p;
+    }
+
+    public char getRepresentation() {
+        return representation;
+    }
 }
--- src/test/java/softeer2nd/domain/PawnTest.java
+++ src/test/java/softeer2nd/domain/PawnTest.java
@@ -7,26 +7,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
-    private static final ChessColor WHITE = ChessColor.WHITE;
-    private static final ChessColor BLACK = ChessColor.BLACK;
 
     @Test
     @DisplayName("색깔을 가진 폰이 생성되어야 한다.")
     public void createPawn(){
-        verifyPawn(WHITE);
-        verifyPawn(BLACK);
+        verifyPawn(ChessColor.WHITE);
+        verifyPawn(ChessColor.BLACK);
     }
 
     @Test
     @DisplayName("인자가 없을 때 기본으로 흰 색 말을 생성하는지")
     public void createWhitePieceWithNoParameter() {
         Pawn pawn = new Pawn();
-        assertEquals(WHITE, pawn.getColor());
+        assertEquals(ChessColor.WHITE, pawn.getColor());
     }
 
     void verifyPawn(final ChessColor color){
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
+        assertEquals(color.getRepresentation(), pawn.getRepresentation());
     }
 }
 
