bee65aa1139363686be2f752edd0cf850fdf3187
Seonmi Park <seonmi@bagseonmiui-MacBookPro.local>
2023-07-04 17:30:55 +0900

    refactor: 체스판 1차원 배열로 리팩토링

--- src/main/java/softeer2nd/domain/Board.java
+++ src/main/java/softeer2nd/domain/Board.java
@@ -7,37 +7,39 @@ import java.util.List;
 public class Board {
 
     private final int MAX_SIZE = 8;
-    char[][] pieces;
+    String[] pieces;
 
     public Board() {
-        pieces = new char[MAX_SIZE][MAX_SIZE];
+        pieces = new String[MAX_SIZE];
     }
 
     public void initialize() {
         char tmp[] = {'.', 'p', 'P'}, c;
 
         for (int i = 0; i < MAX_SIZE; i++) {
+            StringBuilder sb = new StringBuilder();
             if (i == 1) c = tmp[2];
             else if (i == 6) c = tmp[1];
             else c = tmp[0];
 
-            for (int j = 0; j < MAX_SIZE; j++) {
-                pieces[i][j] = c;
+            for(int j=0;j<MAX_SIZE;j++){
+                sb.append(c);
             }
+            pieces[i]=sb.toString();
         }
     }
 
     public void print() {
-        for(char[] row : pieces){
-            System.out.println(new String(row));
+        for(String row : pieces){
+            System.out.println(row);
         }
     }
 
     public String getWhitePawnsResult() {
-        return new String(pieces[6]);
+        return pieces[6];
     }
 
     public String getBlackPawnsResult() {
-        return new String(pieces[1]);
+        return pieces[1];
     }
 }
