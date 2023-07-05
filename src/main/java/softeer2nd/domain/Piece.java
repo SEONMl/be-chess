4e7eab74ce256fcfff56f81ba3cbdbc0c8fba580
Seonmi Park <seonmi@bagseonmiui-MacBookPro.local>
2023-07-05 11:26:54 +0900

    feat: 각 기물과 색깔 별로 인스턴스를 생성하는 팩토리 메서드 구현

--- src/main/java/softeer2nd/domain/Piece.java
+++ src/main/java/softeer2nd/domain/Piece.java
@@ -16,8 +16,55 @@ public class Piece {
         return new Piece(type, color);
     }
 
+    public static Piece createWhitePawn() {
+        return createPiece(Type.PAWN, Color.WHITE);
+    }
+
+    public static Piece createBlackPawn() {
+        return createPiece(Type.PAWN, Color.BLACK);
+    }
+    public static Piece createWhiteBishop() {
+        return createPiece(Type.BISHOP, Color.WHITE);
+    }
+
+    public static Piece createBlackBishop() {
+        return createPiece(Type.BISHOP, Color.BLACK);
+    }
+    public static Piece createWhiteRook() {
+        return createPiece(Type.ROOK, Color.WHITE);
+    }
+
+    public static Piece createBlackRook() {
+        return createPiece(Type.ROOK, Color.BLACK);
+    }
+    public static Piece createWhiteKnight() {
+        return createPiece(Type.KNIGHT, Color.WHITE);
+    }
+
+    public static Piece createBlackKnight() {
+        return createPiece(Type.KNIGHT, Color.BLACK);
+    }
+    public static Piece createWhiteKing() {
+        return createPiece(Type.KING, Color.WHITE);
+    }
+
+    public static Piece createBlackKing() {
+        return createPiece(Type.KING, Color.BLACK);
+    }
+
+    public static Piece createWhiteQueen() {
+        return createPiece(Type.QUEEN, Color.WHITE);
+    }
+
+    public static Piece createBlackQueen() {
+        return createPiece(Type.QUEEN, Color.BLACK);
+    }
+
     public Color getColor() {
         return color;
     }
 
+    public Type getType() {
+        return type;
+    }
 }
--- src/test/java/softeer2nd/domain/PieceTest.java
+++ src/test/java/softeer2nd/domain/PieceTest.java
@@ -6,18 +6,42 @@ import softeer2nd.domain.VO.Color;
 import softeer2nd.domain.VO.Type;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PieceTest {
 
     @Test
     @DisplayName("색깔을 가진 폰이 생성되어야 한다.")
-    public void createPawn(){
+    public void createPawn() {
         verifyPawn(Type.PAWN, Color.WHITE);
         verifyPawn(Type.KING, Color.BLACK);
     }
 
-    void verifyPawn(final Type type, final Color color){
+    void verifyPawn(final Type type, final Color color) {
         Piece piece = Piece.createPiece(type, color);
         assertThat(piece.getColor()).isEqualTo(color);
+    }
+
+    @Test
+    @DisplayName("기물과 색깔 별로 인스턴스 생성되는지?")
+    void createPiece() {
+        verifyPiece(Piece.createWhitePawn(), Type.PAWN, Color.WHITE);
+        verifyPiece(Piece.createWhiteKing(), Type.KING, Color.WHITE);
+        verifyPiece(Piece.createWhiteBishop(), Type.BISHOP, Color.WHITE);
+        verifyPiece(Piece.createWhiteKnight(), Type.KNIGHT, Color.WHITE);
+        verifyPiece(Piece.createWhiteQueen(), Type.QUEEN, Color.WHITE);
+        verifyPiece(Piece.createWhiteRook(), Type.ROOK, Color.WHITE);
+
+        verifyPiece(Piece.createBlackPawn(), Type.PAWN, Color.BLACK);
+        verifyPiece(Piece.createBlackKing(), Type.KING, Color.BLACK);
+        verifyPiece(Piece.createBlackBishop(), Type.BISHOP, Color.BLACK);
+        verifyPiece(Piece.createBlackKnight(), Type.KNIGHT, Color.BLACK);
+        verifyPiece(Piece.createBlackQueen(), Type.QUEEN, Color.BLACK);
+        verifyPiece(Piece.createBlackRook(), Type.ROOK, Color.BLACK);
+    }
+
+    void verifyPiece(Piece piece, Type type, Color color) {
+        assertEquals(color, piece.getColor());
+        assertEquals(type, piece.getType());
     }
 }
