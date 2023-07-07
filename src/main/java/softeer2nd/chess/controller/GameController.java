6b68ad662d397ab048aed0793a35148693699a5b
Park Seonmi <sm000117@naver.com>
2023-07-07 13:02:13 +0900

    feat: 기물 이동 move 명령 추가

--- src/main/java/softeer2nd/chess/controller/GameController.java
+++ src/main/java/softeer2nd/chess/controller/GameController.java
@@ -4,7 +4,10 @@ import softeer2nd.chess.domain.Board;
 
 import java.util.Scanner;
 
+import static softeer2nd.chess.utils.StringUtils.SPACE;
+
 public class GameController {
+    private final String MOVE_COMMAND = "move";
     private Board chessBoard;
     private Scanner sc;
     private int round;
@@ -26,10 +29,23 @@ public class GameController {
         while (true) {
             System.out.print(round % 2 + 1 + "님 입력 >>> ");
             round++;
-            int i = sc.nextInt();
+            String cmd = sc.nextLine();
+            if (cmd.equals("0")) break;
 
-            if (i == 0) break;
+            isStartWithMove(cmd);
+
+            System.out.println(chessBoard.show());
         }
         System.out.println("게임이 종료됐습니다.");
+    }
+    private void isStartWithMove(String command) {
+        if(command.startsWith(MOVE_COMMAND)) {
+            String[] splitCommand  = command.split(SPACE);
+            commandMove(splitCommand[1], splitCommand[2]);
+        }
+    }
+
+    private void commandMove(String srcPosition, String dstPosition) {
+        chessBoard.move(srcPosition, dstPosition);
     }
 }
