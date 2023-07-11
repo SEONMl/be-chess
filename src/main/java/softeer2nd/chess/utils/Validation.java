e4394cddc1733a435dfbca659a1a156c23cc199d
Park Seonmi <sm000117@naver.com>
2023-07-11 11:24:37 +0900

    fix: 잘못 선언된 Exception 수정

--- src/main/java/softeer2nd/chess/utils/Validation.java
+++ src/main/java/softeer2nd/chess/utils/Validation.java
@@ -3,12 +3,13 @@ package softeer2nd.chess.utils;
 import softeer2nd.chess.domain.Board;
 
 public class Validation {
+    private Validation() {}
 
-    public static void isOutOfBoard(String command) throws IllegalAccessException {
+    public static void isOutOfBoard(String command) throws IllegalArgumentException {
         int column = command.charAt(0) - 'a';
         int row = Board.MAX_SIZE - (command.charAt(1) - '0');
         if (notInBound(column) || notInBound(row)) {
-            throw new IllegalAccessException("잘못된 이동 명령을 내렸습니다.");
+            throw new IllegalArgumentException("잘못된 이동 명령을 내렸습니다.");
         }
     }
 
@@ -17,15 +18,15 @@ public class Validation {
     }
 
 
-    public static void NOT_A_MOVE_COMMAND() throws IllegalAccessException {
-        throw new IllegalAccessException("이동을 하지 않는 명령입니다.");
+    public static void NOT_A_MOVE_COMMAND() throws IllegalArgumentException {
+        throw new IllegalArgumentException("이동을 하지 않는 명령입니다.");
     }
 
-    public static void THROW_ALREADY_PIECE_EXIST() throws IllegalAccessException {
-        throw new IllegalAccessException("동선에 기물이 존재하여 이동할 수 없습니다.");
+    public static void THROW_ALREADY_PIECE_EXIST() throws IllegalArgumentException {
+        throw new IllegalArgumentException("동선에 기물이 존재하여 이동할 수 없습니다.");
     }
 
-    public static void NOT_ALLOW_DIRECTION() throws IllegalAccessException {
-        throw new IllegalAccessException("이동할 수 없는 방향입니다.");
+    public static void NOT_ALLOW_DIRECTION() throws IllegalArgumentException {
+        throw new IllegalArgumentException("이동할 수 없는 방향입니다.");
     }
 }
